57711030fec223ecfcf81d49f4331adc5416f57c
ziyinq <ziyinq@seas.upenn.edu>
2017-10-01 13:29:00 -0400

    add read text file

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 // Created by ziyinqu on 10/1/17.
 //
 
-#include <stdio.h>
+#include "readfile.h"
 
 int main(){
+    std::string file = "sparseDragonSamples.txt";
+    std::vector<Vector3f> xp;
+    std::vector<Vector3f> vp;
+    readtxt(file, xp);
+    int size = xp.size();
+    vp.resize(size);
     return 0;
 }
---
+++ readfile.h
@@ -0,0 +1,32 @@
+//
+// Created by ziyinqu on 10/1/17.
+//
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "Eigen/Eigen"
+
+#ifndef MPM_READFILE_H
+#define MPM_READFILE_H
+
+using namespace Eigen;
+
+void readtxt(std::string filename, std::vector<Vector3f>& xp){
+    std::ifstream inputfile;
+    inputfile.open(filename);
+    if(!inputfile){
+        std::cerr << "Unable to open " << filename << "!" << std::endl;
+        exit(1);
+    }
+    std::string line;
+    while(std::getline(inputfile, line)){
+        std::stringstream ss(line);
+        Vector3f thisXp;
+        for (int i = 0; i < 3; i++){
+            ss >> thisXp(i);
+        }
+        xp.push_back(thisXp);
+    }
+}
+#endif //MPM_READFILE_H
