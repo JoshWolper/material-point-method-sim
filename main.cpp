9e166bbd15956dbf4482bb94f556f3c45da3d3eb
Josh Wolper <joshwolper@gmail.com>
2017-10-11 16:41:43 -0400

    Added saving routine to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include "mpmInitialize.h"
 #include "transfer.h"
 
+void saveParticles(vector<Vector3f> points);
+
 int main(){
 
     // MPM simulation parameters setting up
@@ -35,5 +37,35 @@ int main(){
 
     // transfer from Grid to particles
     transferG2P(particles, gridAttrs, gridInfo, dt, alpha);
+
+    //Save the particles!
+    vector<Vector3f> points;
+    for(int i = 0; i < particles.size(); i++){
+	points.push_back(particles[i].posP); //add the positions to the points list!
+    }
+    saveParticles(points); //call the saving routine
+
     return 0;
+}
+
+void saveParticles(vector<Vector3f> points){
+
+    //Write to an object file
+    ofstream outfile;
+    
+    //later we'll want a second parameter, timeStep to make it spit out diferently named files!
+    //String filename = "sim_t=" + itos(
+
+    outfile.open("simOut.obj");
+
+    for(int i = 0; i < points.size(); i++){
+        double point [3];
+        point[0] = points[i][0];
+        point[1] = points[i][1];
+        point[2] = points[i][2];
+        outfile << "v " << point[0] << " " << point[1] << " " << point[2] << "\n";
+    }
+    outfile.close();
+
+    return;
 }
